7c9b6869d098b72cb9dda27cbad0bfbcbac03c43
Luchillo <luchillo17@gmail.com>
2018-03-16 09:22:05 -0500

    [Chore] Extend app state with feature states

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -7,9 +7,13 @@ import {
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import * as fromTracks from './tracks/tracks.reducer';
+import * as fromFilter from './filter/filter.reducer';
+import * as fromFavorites from './favorites/favorites.reducer';
 
-export interface State
-  extends fromTracks.State {}
+export interface State extends
+  fromTracks.State,
+  fromFilter.State,
+  fromFavorites.State {}
 
 export const reducers: ActionReducerMap<State> = {
 
